d30cc825d1636ba61335e6b9f96ba551b7e85e66
subot <vlpolj@ttu.ee>
2020-10-02 15:42:03 +0300

    Just learn smth about ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from "@angular/forms";
+import { RouterModule, Routes} from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FooterComponent } from './footer/footer.component';
 import { MoviesComponent } from './movies/movies.component';
+import { Movies1Component } from './movies1/movies1.component';
+
+const appRoutes: Routes = [
+  {path: '', component:HomePageComponent},
+  {path: 'movie', component:MoviesComponent},
+  {path: 'about', component:Movies1Component}
+]
 
 @NgModule({
   declarations: [
@@ -16,13 +24,15 @@ import { MoviesComponent } from './movies/movies.component';
     NavbarComponent,
     HomePageComponent,
     FooterComponent,
-    MoviesComponent
+    MoviesComponent,
+    Movies1Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
