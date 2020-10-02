a5a9fe8e8ccdcf277be6a264bbd67821a4471472
subot <vlpolj@ttu.ee>
2020-10-02 19:17:17 +0300

    Try connect back and front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from "@angular/forms";
 import { RouterModule, Routes} from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
 
+// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,7 +34,9 @@ const appRoutes: Routes = [
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
