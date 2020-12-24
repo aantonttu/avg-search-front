b00401b3d93cb50c0a214aa7581e354b313493b6
German Hanmamedov <gehanm@ttu.ee>
2020-12-24 03:59:00 +0300

    registration connected to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule, Routes} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -36,7 +36,8 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+import {UserPassword} from '../user-password';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  checkoutForm;
+  url = 'api/users/register';
+  userPassword: UserPassword;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+  }
+
+  // tslint:disable-next-line:typedef
+  onSubmit(userPassword) {
+    console.log(userPassword);
+    this.http.post(this.url,
+      {
+        username: userPassword.username,
+        password: userPassword.password
+      }
+    ).subscribe(
+      () => {
+        console.log('Registration successful!');
+        this.checkoutForm.reset();
+      },
+      error => {
+        console.log('Registration failed!');
+        console.log(error);
+      }
+    );
   }
 
 }
