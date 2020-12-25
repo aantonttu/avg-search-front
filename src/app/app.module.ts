d68eeb8d2b4a3433d6057ce9ec81747171c1203f
German Hanmamedov <gehanm@ttu.ee>
2020-12-25 19:07:53 +0300

    added interceptors and added login logic, also can logout after login (navbar buttons).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { RouterModule, Routes} from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 // import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { FooterComponent } from './footer/footer.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {FooterComponent} from './footer/footer.component';
+import {MovieDetailsComponent} from './movie-details/movie-details.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {JwtInterceptor} from './jwt.interceptor';
+import {ErrorInterceptor} from './error.interceptor';
 
 
 const appRoutes: Routes = [
@@ -38,9 +40,12 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
     ReactiveFormsModule
-
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,14 +13,27 @@ export class NavbarComponent implements OnInit {
   genres: any[];
   url = 'api/movies/genres';
 
-  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) { }
+  constructor(private http: HttpClient,
+              private activatedRoute: ActivatedRoute,
+              private authService: AuthService) {
+  }
 
   ngOnInit(): void {
     this.getAllGenres();
   }
 
   // tslint:disable-next-line:typedef
-  getAllGenres(){
+  checkLogin() {
+    return this.authService.currentUserValue == null;
+  }
+
+  // tslint:disable-next-line:typedef
+  logout() {
+    this.authService.logout();
+  }
+
+  // tslint:disable-next-line:typedef
+  getAllGenres() {
     this.http.get<any>(this.url)
       .subscribe(data => {
         this.genres = data;
